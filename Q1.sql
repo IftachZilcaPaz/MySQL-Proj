0b4dd8b8f63e2537442e9419c11f13111519e11c
IftachZilcaPaz <151572520+IftachZilcaPaz@users.noreply.github.com>
2023-11-20 22:03:28 +0200

    Update Q1.sql

--- Q1.sql
+++ Q1.sql
@@ -1,3 +1,27 @@
+# q1.sql: ​Make a query to create the following tables:
+#
+#
+# pet​ - represents an animal in the shelter or that was adopted from the shelter
+# ● primary key: id
+# ● other columns: kind ("dog"/"cat"/"bird"/etc), color, birth_date, arrival_date
+#
+#
+# owner​ - represents people that adopt animals from the shelter
+# ● primary key: id
+# ● other columns: fname lname gender
+#
+#
+# adoption​ - represents what owner adopted what animal (the relations is one to many)
+# ● Primary Key is the combination of both ​owner_id​ and ​pet_id
+# ● owner_id​ references ​id ​from the ​owner​ table.
+# ● pet_id​ references ​id ​from the ​pet​ table. must be unique.
+# ● other columns: ​adoption_date
+#
+#
+#IMPORTANT: see and execute ​insert_shelter_data.sql​ to add values to the table.
+
+
+
 CREATE SCHEMA pet_shelter;
 
 use pet_shelter;
